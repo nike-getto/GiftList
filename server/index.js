f1063b23800d62eadce5f3aa038057adbe7e39b2
nike-getto <savastanisic7@gmail.com>
2023-03-08 23:23:27 +0100

    Solution applied...

--- server/index.js
+++ server/index.js
@@ -1,29 +1,32 @@
-const express = require('express');
-const verifyProof = require('../utils/verifyProof');
+const express = require('express')
+const verifyProof = require('../utils/verifyProof')
+const niceList = require('../utils/niceList.json')
+const MerkleTree = require('../utils/MerkleTree')
 
-const port = 1225;
+const port = 1225
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const merkleTree = new MerkleTree(niceList)
+const root = merkleTree.getRoot()
+const MERKLE_ROOT = root
 
 app.post('/gift', (req, res) => {
-  // grab the parameters from the front-end here
-  const body = req.body;
+	// grab the parameters from the front-end here
+	const body = req.body
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
-    res.send("You got a toy robot!");
-  }
-  else {
-    res.send("You are not on the list :(");
-  }
-});
+	// TODO: prove that a name is in the list
+	const isInTheList = verifyProof(body.proof, body.name, MERKLE_ROOT)
+	if (isInTheList) {
+		res.send('You got a toy robot!')
+	} else {
+		res.send('You are not on the list :(')
+	}
+})
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}!`);
-});
+	console.log(`Listening on port ${port}!`)
+})
