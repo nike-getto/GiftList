7718de62ca7c73366d5b8151dccbc6b6e3d2c5a2
nike-getto <savastanisic7@gmail.com>
2023-03-18 19:38:46 +0100

    solved...

--- client/index.js
+++ client/index.js
@@ -7,7 +7,7 @@ const serverUrl = 'http://localhost:1225'
 async function main() {
 	// TODO: how do we prove to the server we're on the nice list?
 	const merkleTree = new MerkleTree(niceList)
-	const name = 'Nike Getto'
+	const name = 'Hristina Stojanovic Preslatko'
 	const index = niceList.findIndex((n) => n === name)
 	const proof = merkleTree.getProof(index)
 
